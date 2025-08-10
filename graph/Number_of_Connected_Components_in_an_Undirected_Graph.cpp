3f06dbc95f0d4f5aed676e52244f282738b4611c
MantakaMahir <mantakamahir@gmail.com>
2025-08-10 17:17:18 +0600

    Updated DSA solutions

--- graph/Number_of_Connected_Components_in_an_Undirected_Graph.cpp
+++ graph/Number_of_Connected_Components_in_an_Undirected_Graph.cpp
@@ -10,138 +10,75 @@ using namespace std;
 
 // problem link: https://neetcode.io/problems/count-connected-components
 
-// In this problem, we need to find how many connected components are in a graph.
-// A connected component is defined as:
-// "A set of nodes where every pair has a path between them."
-// That means each node in a component is reachable from any other node in the same component.
-
-// There are three common methods to solve this problem:
-// 1) DFS (Depth First Search)
-// 2) BFS (Breadth First Search)
-// 3) Union-Find (Disjoint Set Union - DSU)
-
-// Both DFS and BFS work fine, but DFS might cause recursion depth issues (stack overflow) on very large inputs.
-// BFS is usually safer in terms of recursion limits since it uses a queue and iterative approach.
-// Union-Find is usually fastest and most efficient for dynamic connectivity problems.
-
-// Let's see how BFS works for this problem:
-
-/*
-BFS approach:
-- We create an adjacency list from the edges for easy traversal.
-- We maintain a visited array to keep track of visited nodes.
-- For each node:
-    - If it is not visited:
-        - Run BFS starting from this node, which will visit all nodes in this connected component.
-        - Increase the count of connected components by 1.
-- Finally, return the count of connected components.
-*/
-
-// BFS solution
-class SolutionBFS
+class Solution
 {
 public:
     int countComponents(int n, vector<vector<int>> &edges)
     {
-        // Build adjacency list
         vector<vector<int>> adj(n);
-        vector<bool> visit(n, false);
+        int visited[n] = {0};
+        // converting the edges to adj list
         for (const auto &edge : edges)
         {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        int res = 0; // to count connected components
-
-        // Check each node
-        for (int node = 0; node < n; ++node)
+        int counting = 0;
+        for (int node = 0; node < n; node++)
         {
-            if (!visit[node])
+            if (!visited[node])
             {
-                bfs(adj, visit, node); // visit all connected nodes from this node
-                res++;                 // found one connected component
+                dfs(node, visited, adj); // both
+                bfs(node, adj, visited); // both work
+                counting++;
             }
         }
-        return res;
+        return counting;
     }
-
-private:
-    void bfs(vector<vector<int>> &adj, vector<bool> &visit, int node)
+    void bfs(int node, vector<vector<int>> &adj, int visited[])
     {
+        int n = adj.size();
         queue<int> q;
+        vector<int> bfs;
         q.push(node);
-        visit[node] = true;
-
+        visited[node] = 1;
         while (!q.empty())
         {
-            int cur = q.front();
+            int node = q.front();
             q.pop();
-            // Visit all neighbors of current node
-            for (int nei : adj[cur])
+            bfs.push_back(node);
+            for (auto itr : adj[node])
             {
-                if (!visit[nei])
+                if (!visited[itr])
                 {
-                    visit[nei] = true;
-                    q.push(nei);
+                    visited[itr] = 1;
+                    q.push(itr);
                 }
             }
         }
     }
-};
-
-/*
-DFS approach:
-- Similar to BFS, but uses recursion instead of a queue.
-- From an unvisited node, recursively visit all its neighbors.
-- Recursion explores each branch as deep as possible before backtracking.
-- Each time we start DFS from an unvisited node, it means we found a new connected component.
-- Return the count of connected components.
-- Note: DFS can cause stack overflow for very large graphs if recursion is too deep.
-*/
-
-class SolutionDFS
-{
-public:
-    int countComponents(int n, vector<vector<int>> &edges)
+    void dfso(int node, vector<int> &ls, vector<vector<int>> &adj, int visited[])
     {
-        // Build adjacency list
-        vector<vector<int>> adj(n);
-        vector<bool> visit(n, false);
-        for (const auto &edge : edges)
+        visited[node] = 1;
+        ls.push_back(node);
+        for (auto itr : adj[node])
         {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-
-        int res = 0; // connected components count
-
-        // Check all nodes
-        for (int node = 0; node < n; ++node)
-        {
-            if (!visit[node])
+            if (!visited[itr])
             {
-                dfs(adj, visit, node); // visit all connected nodes recursively
-                res++;                 // one more connected component found
-            }
-        }
-        return res;
-    }
-
-private:
-    void dfs(const vector<vector<int>> &adj, vector<bool> &visit, int node)
-    {
-        visit[node] = true; // mark current node visited
-        for (int nei : adj[node])
-        {
-            if (!visit[nei])
-            {
-                dfs(adj, visit, nei); // recursively visit neighbors
+                dfso(itr, ls, adj, visited);
             }
         }
     }
+    void dfs(int node, int visited[], vector<vector<int>> &adj)
+    {
+        visited[node] = 1;
+        vector<int> ls;
+        dfso(node, ls, adj, visited);
+    }
 };
-
 /*
 Union-Find (Disjoint Set Union - DSU) approach:
 - Initialize each node as its own parent (separate set).
